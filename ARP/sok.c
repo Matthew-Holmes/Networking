bb3623368fd75ae67efb67b6f3e19a99d48985bd
Matthew <matthew.holmes111@gmail.com>
2024-10-24 20:59:28 +0100

    socket creation

--- ARP/sok.c
+++ ARP/sok.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 int main()
 {
-    printf("hello sockets\n");
+
+    int sockfd;
+    struct sockaddr_in server_addr;    
+    char *packet = "hello!";
+
+    // create the UDP socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    printf("created socket.\naddress family: internet\ncode: %d\n", AF_INET);
+    printf("and with SOCK_DGRAM: %d\n", SOCK_DGRAM);
+
+    close(sockfd);
+
+    printf("socket closed");
+
     return 0;
 }
